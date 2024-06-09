e65c6cad4d55da24312eafdd7d9a020e9911aa91
talshturm <talshturm23@gmail.com>
2024-06-09 11:23:58 +0300

    feat(productRouter.ts): product functions added

--- Server/routers/productRouter.ts
+++ Server/routers/productRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getAll, getById } from "../controllers/productContoller";
+import { getAll, getById, deleteProduct, updateProduct, createProduct } from "../controllers/productContoller";
 
 const router = Router();
 
 router.get("/", getAll);
 router.get("/:id", getById);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
