c8bc40b2c4b75f762c355db11f97a70c5830d96c
talshturm <talshturm23@gmail.com>
2024-06-10 15:12:56 +0300

    feat(navbar.vue): component added

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,35 +1,42 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #f8dede;">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">
+        <img src='../assets/foxIcon.png' width="50">
+      </router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/" style="color: #555;">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/products" style="color: #555;">Products</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/cart" style="color: #555;">Cart</router-link>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-secondary" type="submit">Search</button>
+        </form>
+      </div>
     </div>
   </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+/* Custom styling for the navbar */
+.navbar-toggler-icon {
+  background-color: #555;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index'
 import store from './store/index';
+import 'bootstrap/dist/css/bootstrap.css';
+import { IconsPlugin } from 'bootstrap-vue';
+
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
